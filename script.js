479229ba219d04b91b3ad709cd903d2a2902a5a7
Christian <christianaguilar2223@gmail.com>
2024-03-23 17:40:16 -0400

    added random rgb function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-//get elemets from html
-
-//establish variables
+// html elements
+const gameDisplay = document.querySelector(".game-grid")
+const rgbBtn = document.getElementById("#rgb-radio")
+const monochromeBtn = document.getElementById("#Monochrome-radio")
+const resetBtn = document.querySelector("reset")
 
 //create function for grid input
-
+function getGridSize () {
+    let gridSize = prompt("Enter a number 1-100")
+}
 //rgb function
-
+function useRGB () {
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    return `rgb(${r},${g},${b})`
+}
 //monochrome function
 
 //reset function
